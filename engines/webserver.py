0ae173b354a255ce96a277002153a94bfae7b790
Muhammad Waqar <>
2019-12-25 22:27:52 -0500

    Sweet! Made a Flask webserver that had an API to read in JSON file and write it to disk.

---
+++ engines/webserver.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'about': "hello world"}
+
+    def post(self):
+        some_json=request.get_json()
+        return {'you sent': some_json }, 201
+
+class Multi(Resource):
+    def get(self, num):
+        return {'result': num*10}
+
+
+class MessageApp(Resource):
+    def post(self):
+        myMessage=request.get_json()
+        with open('engines/message_json/latest_message.json', 'w', encoding='utf-8') as f:
+            json.dump(myMessage, f, ensure_ascii=False, indent=4)
+        return {'you sent': myMessage }, 201
+
+
+api.add_resource(HelloWorld,'/')
+api.add_resource(Multi, '/multi/<int:num>')
+api.add_resource(MessageApp,'/MessageApp')
+
+if __name__ == '__main__':
+    app.run(debug=True)
